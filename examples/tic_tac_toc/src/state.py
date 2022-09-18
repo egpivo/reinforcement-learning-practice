17af41156f5681a506b0069b56b7084bf93dc6ec
Wen-Ting Wang <egpivo@gmail.com>
2022-09-18 22:06:34 +0800

    CHG: reorganize the summarize function for readability

--- examples/tic_tac_toc/src/state.py
+++ examples/tic_tac_toc/src/state.py
@@ -2,21 +2,16 @@ import numpy as np
 from src import BOARD_COLS, BOARD_ROWS, BOARD_SIZE, DRAW, PLAYER1, PLAYER2
 
 
-def summarize_game(data: dict) -> list:
-    results = []
-    for i in range(BOARD_ROWS):
-        results.append(np.sum(data[i, :]))
-    for j in range(BOARD_COLS):
-        results.append(np.sum(data[:, j]))
-
+def summarize_game(data: np.array) -> tuple:
+    row_sums = np.sum(data, axis=1)
+    column_sums = np.sum(data, axis=0)
     diagonal_sum = np.trace(data)
     anitdiagonal_sum = np.trace(data[:, ::-1])
-    results.append(diagonal_sum)
-    results.append(anitdiagonal_sum)
-    return results
+
+    return (*row_sums, *column_sums, diagonal_sum, anitdiagonal_sum)
 
 
-def set_winner(data: dict) -> int:
+def set_winner(data: np.array) -> int:
     for result in summarize_game(data):
         if abs(result) == 3:
             return PLAYER1 if result > 0 else PLAYER2
